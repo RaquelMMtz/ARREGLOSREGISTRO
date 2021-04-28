6e6fbfcc391a43298292e0dc1bf0fb783859763a
RaquelMMtz <70656439+RaquelMMtz@users.noreply.github.com>
2021-04-27 22:12:44 -0500

    Add files via upload

---
+++ ArreglosRegistros/CapturaBoxBuscarExistencia.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ArreglosRegistros
+{
+    public partial class CapturaBoxBuscarExistencia : Form
+    {
+        public CapturaBoxBuscarExistencia()
+        {
+            InitializeComponent();
+        }
+        public string existenciaproducto
+        {
+            get { return (textBox1.Text); }
+        }
+        private void CapturaBoxBuscarExistencia_Load(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox1.Focus();
+        }
+    }
+}
---
+++ ArreglosRegistros/DialogBoxCaptura.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ArreglosRegistros
+{
+    public partial class DialogBoxCaptura : Form
+    {
+        public DialogBoxCaptura()
+        {
+            InitializeComponent();
+        }
+
+        // declaracionb de metodos get
+        public string id
+        {
+            get { return (textBox1.Text); }
+        }
+        public string nombreproducto
+        {
+            get { return (textBox2.Text); }
+        }
+        public string precioproducto
+        {
+            get { return (textBox3.Text); }
+        }
+        public string existenciaproducto
+        {
+            get { return (textBox4.Text); }
+        }
+        private void DialogBoxCaptura_Load(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+            textBox4.Clear();
+            textBox1.Focus();
+        }
+    }
+}
